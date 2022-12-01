7b9ffc3facc9e1b1a9a05b5dca9c2b158697b95e
Ben Burke <benkburke@gmail.com>
2022-12-01 23:40:25 +0000

    Update 3 files

--- src/observables.test.js
+++ src/observables.test.js
@@ -1,16 +1,24 @@
 import Observable from "./Shared/Observable";
-// import { observe, observable, computed } from "mobx";
+import { observe, makeAutoObservable } from "mobx";
+import { BehaviorSubject } from "rxjs";
 // import { Subject, BehaviorSubject } from "rxjs";
 
-class Person {}
+class Person {
+  constructor() {
+    // Just call it here
+    makeAutoObservable(this);
+  }
+
+  age = null;
+}
 
 it("pub/sub with normal observable", () => {
   let person = new Person();
 
   var personObservable = new Observable(person);
-  personObservable.value = 37;
+  personObservable.value = 30;
 
-  personObservable.subscribe(value => {
+  personObservable.subscribe((value) => {
     console.log(value);
   });
 
@@ -19,6 +27,22 @@ it("pub/sub with normal observable", () => {
   personObservable.notify();
 });
 
-it("pub/sub with Mobx FRP library", () => {});
+it("pub/sub with Mobx FRP library", () => {
+  let person = new Person();
 
-it("pub/sub with RXJS FRP library", () => {});
+  observe(person, "age", (obj) => {
+    console.log(obj.newValue);
+  });
+
+  person.age = 37;
+  person.age = 47;
+});
+
+it("pub/sub with RXJS FRP library", () => {
+  let subject = new BehaviorSubject(50);
+  subject.subscribe((person) => {
+    console.log(person);
+  }, true);
+
+  subject.next(60);
+});
